acb34d0b5c08182ce69671b799201beed67ff67d
Rodney Hovley <Rodney Hovley>
2016-10-20 17:33:02 -0600

    initial commit

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+Template for Node server connected to mongo db backend
+
+Recommended to connect to AdminMongo for GUI interactions with the database
---
+++ config/db.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+        url: {
+        	production : 'mongodb://rphovley:Megarice10@ds021761.mlab.com:21761/default',
+        	local : 'mongodb://localhost:27017/default'
+        } 
+    }
